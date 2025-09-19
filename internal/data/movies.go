c2ff73210de0a1814df112fc27c95b42ad08a05c
gitavk <3apa3ka3@gmail.com>
2025-09-19 17:33:48 +0200

    Ecoding struct.

---
+++ internal/data/movies.go
@@ -0,0 +1,15 @@
+package data
+
+import (
+	"time"
+)
+
+type Movie struct {
+	ID        int64     `json:"id"`
+	CreatedAt time.Time `json:"-"` // Use the - directive
+	Title     string    `json:"title"`
+	Year      int32     `json:"year,omitzero"`    // Add the omitzero directive
+	Runtime   int32     `json:"runtime,omitzero"` // Add the omitzero directive
+	Genres    []string  `json:"genres,omitzero"`  // Add the omitzero directive
+	Version   int32     `json:"version"`
+}
