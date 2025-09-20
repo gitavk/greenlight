f3c2c916a36af3ec5aef1c4d1e1da32397c93e96
gitavk <3apa3ka3@gmail.com>
2025-09-20 10:58:39 +0200

    Sending error messages.

--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -21,7 +21,7 @@ func (app *application) showMovieHandler(w http.ResponseWriter, r *http.Request)
 
 	id, err := app.readIDParam(r)
 	if err != nil {
-		http.NotFound(w, r)
+		app.notFoundResponse(w, r)
 		return
 	}
 	// Create a new instance of the Movie struct, containing the ID we extracted from
@@ -39,7 +39,6 @@ func (app *application) showMovieHandler(w http.ResponseWriter, r *http.Request)
 	// Encode the struct to JSON and send it as the HTTP response.
 	err = app.writeJSON(w, http.StatusOK, envelope{"movie": movie}, nil)
 	if err != nil {
-		app.logger.Error(err.Error())
-		http.Error(w, "The server encountered a problem and could not process your request", http.StatusInternalServerError)
+		app.serverErrorResponse(w, r, err)
 	}
 }
