e38c2e50135c5965456a7ba3a7f79ae2326467cc
gitavk <3apa3ka3@gmail.com>
2025-09-20 09:59:56 +0200

    Customizing JSON.

--- internal/data/movies.go
+++ internal/data/movies.go
@@ -9,7 +9,7 @@ type Movie struct {
 	CreatedAt time.Time `json:"-"` // Use the - directive
 	Title     string    `json:"title"`
 	Year      int32     `json:"year,omitzero"`    // Add the omitzero directive
-	Runtime   int32     `json:"runtime,omitzero"` // Add the omitzero directive
+	Runtime   Runtime   `json:"runtime,omitzero"` // Add the omitzero directive
 	Genres    []string  `json:"genres,omitzero"`  // Add the omitzero directive
 	Version   int32     `json:"version"`
 }
