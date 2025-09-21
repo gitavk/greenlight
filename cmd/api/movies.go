841e98ed0a7bd8ade2571e0b37b6dbf033ef7f57
gitavk <3apa3ka3@gmail.com>
2025-09-21 11:56:33 +0200

    Validating JSON input

--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -6,6 +6,7 @@ import (
 	"time"
 
 	"github.com/gitavk/greenlight/internal/data"
+	"github.com/gitavk/greenlight/internal/validator"
 )
 
 // Add a createMovieHandler for the "POST /v1/movies" endpoint. For now we simply
@@ -20,6 +21,20 @@ func (app *application) createMovieHandler(w http.ResponseWriter, r *http.Reques
 	err := app.readJSON(w, r, &input)
 	if err != nil {
 		app.badRequestResponse(w, r, err)
+		return
+	}
+	// Copy the values from the input struct to a new Movie struct.
+	movie := &data.Movie{
+		Title:   input.Title,
+		Year:    input.Year,
+		Runtime: input.Runtime,
+		Genres:  input.Genres,
+	}
+	// Initialize a new Validator instance.
+	v := validator.New()
+	data.ValidateMovie(v, movie)
+	if !v.Valid() {
+		app.failedValidationResponse(w, r, v.Errors)
 		return
 	}
 
--- internal/data/movies.go
+++ internal/data/movies.go
@@ -2,6 +2,8 @@ package data
 
 import (
 	"time"
+
+	"github.com/gitavk/greenlight/internal/validator"
 )
 
 type Movie struct {
@@ -12,4 +14,28 @@ type Movie struct {
 	Runtime   Runtime   `json:"runtime,omitzero"` // Add the omitzero directive
 	Genres    []string  `json:"genres,omitzero"`  // Add the omitzero directive
 	Version   int32     `json:"version"`
+}
+
+func ValidateMovie(v *validator.Validator, m *Movie) {
+	// Use the Check() method to execute our validation checks. This will add the
+	// provided key and error message to the errors map if the check does not evaluate
+	// to true. For example, in the first line here we "check that the title is not
+	// equal to the empty string". In the second, we "check that the length of the title
+	// is less than or equal to 500 bytes" and so on.
+	v.Check(m.Title != "", "title", "must be provided")
+	v.Check(len(m.Title) <= 500, "title", "must not be more than 500 bytes long")
+
+	v.Check(m.Year != 0, "year", "must be provided")
+	v.Check(m.Year >= 1888, "year", "must be greater than 1888")
+	v.Check(m.Year <= int32(time.Now().Year()), "year", "must not be in the future")
+
+	v.Check(m.Runtime != 0, "runtime", "must be provided")
+	v.Check(m.Runtime > 0, "runtime", "must be a positive integer")
+
+	v.Check(m.Genres != nil, "genres", "must be provided")
+	v.Check(len(m.Genres) >= 1, "genres", "must contain at least 1 genre")
+	v.Check(len(m.Genres) <= 5, "genres", "must not contain more than 5 genres")
+	// Note that we're using the Unique helper in the line below to check that all
+	// values in the input.Genres slice are unique.
+	v.Check(validator.Unique(m.Genres), "genres", "must not contain duplicate values")
 }
