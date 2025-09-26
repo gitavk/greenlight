af3b627815e7d5db366e4ad1dd1484f40720e044
gitavk <3apa3ka3@gmail.com>
2025-09-26 20:01:17 +0200

    Filtering lists

--- internal/data/movies.go
+++ internal/data/movies.go
@@ -30,13 +30,15 @@ func (m MovieModel) GetAll(title string, genres []string, filters Filters) ([]*M
 	query := `
         SELECT id, created_at, title, year, runtime, genres, version
         FROM movies
+        WHERE (LOWER(title) = LOWER($1) OR $1 = '')
+        AND (genres @> $2 OR $2 = '{}')
         ORDER BY id`
 
 	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
 	defer cancel()
 	// Use QueryContext() to execute the query. This returns a sql.Rows resultset
 	// containing the result.
-	rows, err := m.DB.QueryContext(ctx, query)
+	rows, err := m.DB.QueryContext(ctx, query, title, pq.Array(genres))
 	if err != nil {
 		return nil, err
 	}
